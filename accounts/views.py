b23aa9ba3daa4995aa145fb9feb343376b50cdc3
eddiebrett <eddiebrett@hotmail.co.uk>
2019-12-31 13:57:12 +0000

    added login.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
-# Create your views here.
+
+def index(request):
+    """Return the index.html file"""
+    return render(request,  'index.html')
+
+def logout(request):
+    """ log the user out """
+    auth.logout(request)
+    messages.success(request, "you have successfully been logged out!")
+    return redirect(reverse('index'))
+
+def login(request):
+    """ return a login page """
+    return render(request, 'login.html')
